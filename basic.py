1425007b0b97883e8f804dff3d6f0eab56d20572
Jaysmita-sahu <95176053+Jaysmita-sahu@users.noreply.github.com>
2021-11-28 20:00:11 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,24 @@
+#sum of two number
+a="23"
+b=32
+a=int(a)
+print( "the value of a+b is",a+b)
+
+#Which one is greater
+b=(23>32)
+c=(23<32)
+print(b)
+print(c)
+
+#type casting
+a="lina"
+b=24
+c=2.5
+print(a)
+print(b)
+print(c)
+print(type(a))
+d=True
+e=None
+print(d)
+print(e)
